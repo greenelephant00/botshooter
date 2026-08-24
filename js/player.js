5368fe919a22423d2bd730a470fd5f7f7e5e02e2
ben <ben@local>
2026-08-24 14:25:27 +0200

    Move Overlord, Phantom, Artillery, Swarmqueen and Vortex from special bots to regular bot pool

--- js/player.js
+++ js/player.js
@@ -88,16 +88,16 @@ const BOT_TYPES = [
   { name: 'shielder', minScore: 350, minLevel: 5, r: 24, hp: 9,  speed: [0.6, 1],   cooldown: [1800, 2600], pattern: 'wide',   bulletSpeed: 4.5, color: () => `hsl(${Math.floor(Math.random()*20)+90}, 55%, 45%)` },
   { name: 'ghost',    minScore: 400, minLevel: 6, r: 16, hp: 5,  speed: [1.2, 1.8], cooldown: [1800, 2600], pattern: 'teleport', bulletSpeed: 7,   color: () => `hsl(${Math.floor(Math.random()*20)+260}, 55%, 65%)` },
   { name: 'turret',   minScore: 450, minLevel: 7, r: 19, hp: 6,  speed: [0, 0],     cooldown: [333, 333],   pattern: 'turret',   bulletSpeed: 9,   color: () => `hsl(${Math.floor(Math.random()*15)+0}, 80%, 40%)` },
-  { name: 'bomber',   minScore: 550, minLevel: 8, r: 17, hp: 4,  speed: [2.4, 3.0], cooldown: [0, 0],       pattern: 'suicide',  bulletSpeed: 0,   meleeDamage: 35, color: () => `hsl(${Math.floor(Math.random()*20)+30}, 90%, 50%)` }
-];
-
-// Speciale bots: zeldzaam, maar stuk voor stuk zwaar en met een uniek gevecht-gimmick
-const SPECIAL_BOT_TYPES = [
+  { name: 'bomber',   minScore: 550, minLevel: 8, r: 17, hp: 4,  speed: [2.4, 3.0], cooldown: [0, 0],       pattern: 'suicide',  bulletSpeed: 0,   meleeDamage: 35, color: () => `hsl(${Math.floor(Math.random()*20)+30}, 90%, 50%)` },
   { name: 'overlord',   minScore: 700, minLevel: 9,  r: 34, hp: 20, speed: [0.5, 0.8], cooldown: [2600, 3400], pattern: 'megaburst', bulletSpeed: 5,   color: () => `hsl(${Math.floor(Math.random()*15)+290}, 65%, 35%)` },
   { name: 'phantom',    minScore: 700, minLevel: 9,  r: 17, hp: 9,  speed: [1.4, 2.0], cooldown: [1700, 2300], pattern: 'phantom',   bulletSpeed: 0,   meleeDamage: 26, color: () => `hsl(${Math.floor(Math.random()*15)+270}, 70%, 45%)` },
   { name: 'artillery',  minScore: 700, minLevel: 9,  r: 20, hp: 8,  speed: [0.4, 0.7], cooldown: [3200, 4200], pattern: 'mortar',    bulletSpeed: 0,   meleeDamage: 40, color: () => `hsl(${Math.floor(Math.random()*10)+0}, 70%, 32%)` },
   { name: 'swarmqueen', minScore: 700, minLevel: 9,  r: 22, hp: 10, speed: [0.9, 1.3], cooldown: [1400, 2000], pattern: 'triple',    bulletSpeed: 5,   splits: true, color: () => `hsl(${Math.floor(Math.random()*15)+95}, 70%, 40%)` },
-  { name: 'vortex',     minScore: 700, minLevel: 9,  r: 19, hp: 9,  speed: [0.7, 1.1], cooldown: [180, 180],   pattern: 'spiral',    bulletSpeed: 5,   color: () => `hsl(${Math.floor(Math.random()*15)+195}, 80%, 55%)` },
+  { name: 'vortex',     minScore: 700, minLevel: 9,  r: 19, hp: 9,  speed: [0.7, 1.1], cooldown: [180, 180],   pattern: 'spiral',    bulletSpeed: 5,   color: () => `hsl(${Math.floor(Math.random()*15)+195}, 80%, 55%)` }
+];
+
+// Speciale bots: zeldzaam, maar stuk voor stuk zwaar en met een uniek gevecht-gimmick
+const SPECIAL_BOT_TYPES = [
   { name: 'swapper',    minScore: 700, minLevel: 9,  r: 18, hp: 12, speed: [1.0, 1.5], cooldown: [500, 500],   pattern: 'single',    bulletSpeed: 6.5, bulletDmg: 10, swapOnHit: true, color: () => `hsl(${Math.floor(Math.random()*15)+300}, 85%, 45%)` },
   { name: 'sentinel',   minScore: 700, minLevel: 9,  r: 20, hp: 11, speed: [0.6, 1.0], cooldown: [3200, 4000], pattern: 'sentinellaser', bulletSpeed: 0, specialDmg: 50, color: () => `hsl(${Math.floor(Math.random()*15)+185}, 85%, 45%)` },
   { name: 'warden',     minScore: 700, minLevel: 9,  r: 19, hp: 10, speed: [0.9, 1.3], cooldown: [1000, 1000], pattern: 'mine',          bulletSpeed: 0, specialDmg: 26, color: () => `hsl(${Math.floor(Math.random()*15)+25}, 80%, 40%)` },
